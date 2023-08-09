8003cbbdd6edd5d5cf650016400e0039a7564925
ChShreya11 <lakshmishreyach@gmail.com>
2023-08-09 11:30:26 +0530

    react commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -18,8 +18,25 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <h1>Hello World!This is my react app</h1>
-      
+      <h1>Trains Schedule Web App</h1>
+      <h2>Available Trains and their schedules</h2>
+      <h3>Info about available seats and ticket booking:</h3>
+      <label for="tname">Train Name:</label>
+      <input type="text" id="tname" name="tname"></input>
+      <label for="tnumber">Train Number:</label>
+      <input type="text" id="tnumber" name="tnumber"></input>
+      <label for="ttime">Train departure:</label>
+      <input type="text" id="ttime" name="ttime"></input>
+      <label for="tprice">Ticket fare:</label>
+      <input type="text" id="tprice" name="tprice"></input>
+      <label for="tseats">Seats Available:</label>
+      <input type="text" id="tseats" name="tseats"></input>
+      <label for="tdelay">Train is delayed by:</label>
+      <input type="text" id="tdelay" name="tdelay"></input>
+      <label for="tcoach">Train Coach:</label>
+      <input type="text" id="tcoach" name="tcoach"></input>
+      <label for="tclass">Train Coach Class:</label>
+      <input type="text" id="tclass" name="tclass"></input>
     </div>
   );
 }
